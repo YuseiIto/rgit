fcae7ff6cb73a838ef51f7f3a236e0783358eac2
Yusei Ito <yuseikun@Outlook.jp>
2020-03-30 14:48:39 +0900

    Basic file creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        let output = "Hello, world!";
+        let mut ofile = File::create("hello_world.txt").expect("unable to create file");
+        ofile.write_all(output.as_bytes()).expect("unable to write");
+        ofile.flush().expect("Flush failed.")
+    }
 }
