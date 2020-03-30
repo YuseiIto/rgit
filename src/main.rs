b2421220c0c1683dcb85ee529181f3ba67efac7a
Yusei Ito <yuseikun@Outlook.jp>
2020-03-30 20:17:17 +0900

    Add: Read the files and create buffer

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::Read;
 use std::path::PathBuf;
 extern crate dirs;
 
@@ -53,6 +54,34 @@ fn main() {
 
             create_file(&path_base,"info/exclude","# git ls-files --others --exclude-from=.git/info/exclude\n# Lines that start with '#' are comments.\n# For a project mostly in C, the following would be a good set of\n# exclude patterns (uncomment them if you want to use them):\n# *.[oa]\n# *~\n");
             create_file(&path_base, "HEAD", "ref: refs/heads/master");
+        } else if cmd == &String::from("add") {
+            let mut p = PathBuf::from(&path_base);
+
+            let name = match args.get(2) {
+                Some(v) => v,
+                None => panic!("No file specified"),
+            };
+
+            p.push(name);
+
+            let meta = fs::metadata(&p).expect("Failed to get metadata");
+            let size = meta.len();
+
+            let mut head = String::from("blob ");
+            head.push_str(&size.to_string());
+            head.push('\0');
+
+            let current_path = match args.get(0) {
+                Some(v) => v,
+                None => panic!("Couldn't get current path"),
+            };
+
+            let current_path = PathBuf::from(current_path);
+            let mut head = head.into_bytes();
+
+            let mut buf = Vec::new();
+            buf.append(&mut head);
+            read_bytes(&current_path, &name, &mut buf);
         }
     }
 }
@@ -74,4 +103,13 @@ fn dig_dirs(path_base: &PathBuf, dirs: &Vec<&str>) {
         p.push(elm);
         fs::create_dir_all(&p).expect("Dig direcroty failed")
     }
+}
+
+fn read_bytes(path: &PathBuf, name: &str, buf: &mut Vec<u8>) {
+    let mut p = PathBuf::from(&path);
+    p.push(&name);
+    let mut file = std::fs::File::open(&path).expect("file open failed");
+    let mut b = Vec::new();
+    file.read_exact(&mut b).expect("Failed to read the file");
+    buf.append(&mut b);
 }
