22d6a3d7163ce15882551020aaa8b2a91c20b78f
Yusei Ito <yuseikun@Outlook.jp>
2020-03-30 14:58:59 +0900

    Check the argument

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,17 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 1 {
-        let output = "Hello, world!";
-        let mut ofile = File::create("hello_world.txt").expect("unable to create file");
-        ofile.write_all(output.as_bytes()).expect("unable to write");
-        ofile.flush().expect("Flush failed.")
+        let cmd = match args.get(1) {
+            Some(v) => v,
+            None => panic!("No command provided"),
+        };
+
+        if cmd == &String::from("init") {
+            //Init command runned
+            let output = "Hello, world!";
+            let mut ofile = File::create("hello_world.txt").expect("unable to create file");
+            ofile.write_all(output.as_bytes()).expect("unable to write");
+            ofile.flush().expect("Flush failed.")
+        }
     }
 }
