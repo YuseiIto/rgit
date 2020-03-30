f64014ba2fd695a51281864d67caaf74fae2b58c
Yusei Ito <yuseikun@Outlook.jp>
2020-03-30 16:16:10 +0900

    Add: dig dirs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 extern crate dirs;
 
 fn main() {
@@ -27,17 +27,43 @@ fn main() {
         path_base.push("rgit");
         //---
 
-        fs::create_dir(&path_base).expect("Failed to create directory");
-
-        let mut hw_path = PathBuf::from(&path_base);
-        hw_path.push("HelloWorld.txt");
+        path_base.push(".git");
+        fs::create_dir_all(&path_base).expect("Failed to create directory");
 
         if cmd == &String::from("init") {
             //Init command runned
+            let dirs = vec![
+                "hooks",
+                "info",
+                "objects",
+                "objects/info",
+                "objects/pack",
+                "refs",
+                "refs/heads",
+                "refs/tags",
+            ];
+            dig_dirs(&path_base, &dirs);
+
+            let mut hw_path = PathBuf::from(&path_base);
+            hw_path.push("HelloWorld.txt");
             let output = "Hello, world!";
-            let mut ofile = File::create(&hw_path).expect("unable to create file");
-            ofile.write_all(output.as_bytes()).expect("unable to write");
-            ofile.flush().expect("Flush failed.")
+            create_file(&hw_path, &output);
         }
+    }
+}
+
+fn create_file(path: &PathBuf, content: &str) {
+    let mut ofile = File::create(path).expect("unable to create file");
+    ofile
+        .write_all(content.as_bytes())
+        .expect("unable to write");
+    ofile.flush().expect("Flush failed.");
+}
+
+fn dig_dirs(path_base: &PathBuf, dirs: &Vec<&str>) {
+    for elm in dirs {
+        let mut p = PathBuf::from(path_base);
+        p.push(elm);
+        fs::create_dir_all(&p).expect("Dig direcroty failed")
     }
 }
